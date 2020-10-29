ac83fb6ed7bb1c3545bff023c576224a61440d54
Can Rozanes <37912128+canrozanes@users.noreply.github.com>
2020-10-29 10:27:04 -0400

    add tina cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,103 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
+import App from "next/app";
+import { TinaCMS, TinaProvider, useCMS } from "tinacms";
+import {
+  GithubClient,
+  GithubMediaStore,
+  TinacmsGithubProvider,
+} from "react-tinacms-github";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default class Site extends App {
+  cms: TinaCMS;
+
+  constructor(props) {
+    super(props);
+
+    const github = new GithubClient({
+      proxy: "/api/proxy-github",
+      authCallbackRoute: "/api/create-github-access-token",
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+      baseRepoFullName: process.env.NEXT_PUBLIC_REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
+      baseBranch: process.env.NEXT_PUBLIC_BASE_BRANCH, // e.g. 'master' or 'main' on newer repos
+    });
+    console.log(github);
+
+    /**
+     * 1. Create the TinaCMS instance
+     */
+    this.cms = new TinaCMS({
+      enabled: !!props.pageProps.preview,
+      apis: {
+        /**
+         * 2. Register the GithubClient
+         */
+        github,
+      },
+      /**
+       * 3. Register the Media Store
+       */
+      media: new GithubMediaStore(github),
+      /**
+       * 4. Use the Sidebar and Toolbar
+       */
+      sidebar: true,
+      toolbar: true,
+    });
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      /**
+       * 5. Wrap the page Component with the Tina and Github providers
+       */
+      <TinaProvider cms={this.cms}>
+        <TinacmsGithubProvider
+          onLogin={onLogin}
+          onLogout={onLogout}
+          error={pageProps.error}
+        >
+          {/**
+           * 6. Add a button for entering Preview/Edit Mode
+           */}
+          <EditLink />
+          <Component {...pageProps} />
+        </TinacmsGithubProvider>
+      </TinaProvider>
+    );
+  }
 }
 
-export default MyApp;
+const onLogin = async () => {
+  const token = localStorage.getItem("tinacms-github-token") || null;
+  const headers = new Headers();
+
+  if (token) {
+    headers.append("Authorization", "Bearer " + token);
+  }
+
+  const resp = await fetch(`/api/preview`, { headers: headers });
+  const data = await resp.json();
+
+  if (resp.status == 200) window.location.href = window.location.pathname;
+  else throw new Error(data.message);
+};
+
+const onLogout = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload();
+  });
+};
+
+export interface EditLinkProps {
+  cms: TinaCMS;
+}
+
+export const EditLink = () => {
+  const cms = useCMS();
+  console.log(cms);
+  return (
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? "Exit Edit Mode" : "Edit This Site"}
+    </button>
+  );
+};
